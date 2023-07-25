b018a40e0ab4c8f72ee292a34bd4e7867c1ac1f7
Miracle Obinna <mmrealworld@yahoo.com>
2023-07-25 04:50:53 -0700

    shell_project

--- main.h
+++ main.h
@@ -3,10 +3,10 @@
 
 /*---LIBRARIES---*/
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 
 /*---PROTOTYPES---*/
@@ -21,21 +21,21 @@ extern char **environ;
 #define my_sizeof(type) (char *)(&type + 1) - (char *)(&type)
 int main(void);
 
-/* shell_interactive.c */
-char *read_line(void);
-char **split_line(char *line);
-int execute_args(char **args);
-
-/* execute_args */
-int new_process(char **args);
-
 /* shell_no_interactive */
 char *read_stream(void);
 
+/* shell_interactive.c */
+char **split_line(char *line);
+char *read_line(void);
+int execute_args(char **args);
+
 /*---Builtin func---*/
 int cd(char **args);
-int own_exit(char **args);
 int env(char **args);
+int own_exit(char **args);
 int assist(char **args);
+
+/* execute_args */
+int new_process(char **args);
 
 #endif
